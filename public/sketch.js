aa729e3e3da2b3bcf67c222be1e358eb111e57bf
Andrea Valliere <amvalliere@gmail.com>
2017-07-25 17:40:00 -0700

    Created color variables for heart rate levels, plus conditional statement to animate background color accordingly

--- public/sketch.js
+++ public/sketch.js
@@ -3,6 +3,7 @@ function setup() {
   var myCanvas = createCanvas(600, 400);
   myCanvas.parent('display');
   background(200, 200, 200);
+
 }
 
 function draw() {
@@ -17,6 +18,25 @@ function draw() {
   // ellipse(pulse[0], pulse[1], pulse[0] / 2, pulse[1] / 2);
 
   // pulse and BPM
-  background(0, pulse.bpm, 250);
+  // background(0, pulse.bpm, 250);
+
+
+  var high = color(54, 0, 51);
+  var midHigh = color(39, 45, 83);
+  var midLow = color(25, 90, 115);
+  var low = color(11, 135, 147);
+
+
+  if (pulse.bpm < 61) {
+    background(low)
+  } else if (pulse.bpm > 61 && pulse.bpm < 70) {
+    background(midLow)
+  } else if (pulse.bpm > 69 && pulse.bpm < 80) {
+    background(midHigh)
+  } else {
+    background(high)
+  };
+
+  fill(pulse.bpm * 2);
   ellipse(pulse.pulse / 5, pulse.pulse / 5, pulse.pulse / 8, pulse.pulse / 8);
 }
